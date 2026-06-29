ab49a5333aef29bf4cd68584898aaf0e9f14f4e3
Suman Roy <sr9746973@gmail.com>
2026-06-29 19:51:38 +0530

    Day 9: Added payment API endpoint

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import uuid
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "High Throughput Transaction Processing Engine"
+
 
 @app.route("/health")
 def health():
@@ -12,6 +15,23 @@ def health():
         "status": "healthy",
         "service": "Transaction Processing Engine"
     })
+
+
+@app.route("/payment", methods=["POST"])
+def payment():
+
+    data = request.get_json()
+
+    transaction_id = str(uuid.uuid4())
+
+    return jsonify({
+        "transaction_id": transaction_id,
+        "status": "SUCCESS",
+        "sender": data["sender"],
+        "receiver": data["receiver"],
+        "amount": data["amount"]
+    }), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
