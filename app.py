c6fde4f8621e40084fdf2295d649dab44d11a743
Suman Roy <sr9746973@gmail.com>
2026-06-29 15:00:34 +0530

    Day 8: Added health API endpoint

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "High Throughput Transaction Processing Engine"
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy",
+        "service": "Transaction Processing Engine"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
